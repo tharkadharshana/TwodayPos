cfd682d09f9fda3a2595d71a6594c95b09984fb6
Tharka Karunanayake <tharkadharshana@gmail.com>
2025-06-10 03:02:54 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,14 +2,14 @@
 import { initializeApp, getApps, getApp, type FirebaseOptions } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+// Your web app's Firebase configuration
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCzLEbzyEIRJZdEtshKVS0X7JvfKh42F3c",
+  authDomain: "perfectpos.firebaseapp.com",
+  projectId: "perfectpos",
+  storageBucket: "perfectpos.firebasestorage.app",
+  messagingSenderId: "1024884215374",
+  appId: "1:1024884215374:web:5b057c33113af58db5bd1f"
 };
 
 // Initialize Firebase
