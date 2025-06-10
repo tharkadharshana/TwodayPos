b416fc6a43e9eacf96a95a2829b5e0af858770b0
Tharka Karunanayake <tharkadharshana@gmail.com>
2025-06-10 06:57:38 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'static.vecteezy.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
   productionBrowserSourceMaps: false, // Explicitly set, though default is false. This primarily affects production builds.
